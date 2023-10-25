0254737b9ba833c7a24e9ce79e083fcbcf7ed904
abdelmaksoudhassan <62275775+abdelmaksoudhassan@users.noreply.github.com>
2023-10-25 14:54:52 +0300

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const io = require('./socket-io/socket').init(server)
 const port = process.env.PORT || 3000
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
