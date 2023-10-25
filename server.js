b582b2589a7d7cbef38333d7ab2489e262b27ce1
abdelmaksoudhassan <62275775+abdelmaksoudhassan@users.noreply.github.com>
2023-10-25 14:31:07 +0300

    make it better

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const port = process.env.PORT || 3000
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Token');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
