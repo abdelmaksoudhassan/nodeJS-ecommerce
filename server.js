182acb796751f6970cae92b5c7f558b8bb0c0759
abdelmaksoudhassan <62275775+abdelmaksoudhassan@users.noreply.github.com>
2023-10-30 00:14:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
     next();
 });
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/user',userRouter)
 app.use('/admin',adminRouter)
