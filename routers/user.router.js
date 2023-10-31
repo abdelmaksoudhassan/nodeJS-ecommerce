64a5a119ce6835b8d0b388dc2a4c01337a19d65b
abdelmaksoudhassan <62275775+abdelmaksoudhassan@users.noreply.github.com>
2023-10-31 23:57:07 +0200

    Update user.router.js

--- routers/user.router.js
+++ routers/user.router.js
@@ -10,7 +10,7 @@ userRouter.post('/auto-login',userController.autoLogin)
 userRouter.post('/logout',authUser,userController.logOut)
 userRouter.patch('/change-password',authUser,userController.changePassword)
 userRouter.patch('/change-name',authUser,userController.changeName)
-userRouter.patch('/change-photo',authUser,userController.changePhoto)
+userRouter.post('/change-photo',authUser,userController.changePhoto)
 userRouter.delete('/delete-my-account',authUser,userController.deleteMyAccount)
 
 module.exports = userRouter
