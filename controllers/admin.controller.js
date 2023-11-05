ad46a7dc7706a0ccd5b18526baafbec63cf43a76
abdelmaksoudhassan <62275775+abdelmaksoudhassan@users.noreply.github.com>
2023-11-05 17:46:22 +0200

    Update admin.controller.js

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -52,8 +52,7 @@ const autoLogin = async (req,res,next) =>{
         next()
     }
     catch(err){
-        console.log(err)
-        res.status(500).send(err)
+        res.status(401).send(err)
     }
 }
 
